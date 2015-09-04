55e81390472e17f6113385ad2d9d7d85ffc47ada
Anwell Wang <anwellwang@gmail.com>
2015-09-03 21:58:41 -0700

    switch to zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,6 @@ git submodule update --init;
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
